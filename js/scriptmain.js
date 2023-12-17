4c497010cbff828342d6e908e116153cd306e38a
Szybu27 <szymonbu2741@gmail.com>
2023-12-17 16:10:50 +0100

    first commit

---
+++ js/scriptmain.js
@@ -0,0 +1,12 @@
+function goToMain() {
+    window.location.href = "index.html"
+  }
+  function goToDoList() {
+    window.location.href = "todolist.html"
+  }
+  function goToMedia() {
+    window.location.href = "media.html"
+  }
+  function goToDownloads() {
+    window.location.href = "downloads.html"
+  }
